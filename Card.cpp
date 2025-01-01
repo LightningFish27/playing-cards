6e9a5246b0e06a8a11616ad6b53e10c132e0e7fb
Gavin Williams <lightningfish04@gmail.com>
2024-12-31 21:25:49 -0800

    Further implementation progress. Still does not work fully-- deck seems to be full of cards(0,0).

--- Card.cpp
+++ Card.cpp
@@ -1,9 +1,17 @@
 #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <random>
 
 struct Card{
     int value; // The value of the card. Ace is 1.
     int suit; // Hearts, Diamonds, Spades, Clubs
     Card(const int v, const int s) : value(v), suit(s) {}
+
+    friend std::ostream& operator<<(std::ostream& os, Card& card){
+        os << card.value << " of " << card.suit;
+        return os;
+    }
 };
 
 
@@ -20,4 +28,40 @@ public:
             }
         }
     }
+    // Shuffle a deck randomly
+    void shuffle(){
+        static std::random_device rd;
+        std::shuffle(cards.begin(), cards.end(), rd);
+    }
+    // Shuffle a deck semi-randomly. For example, modulus shuffles or chunked shuffles.
+    // MethodComplement will be used for any shuffle types that need an additional variable,
+    // such as how many cards should be in a chunk.
+    void shuffle(int shuffleMethod, int methodComplement){
+        if (shuffleMethod < 0){
+            std::cout << "Attempted to shuffle deck at " << this << " by undefined method.\n";
+            return;
+        }
+        switch(shuffleMethod){
+            case 1:
+                // Modular shuffle
+                break;
+            case 2:
+                // Chunked shuffle
+                break;
+        }
+    }
+
+    // Draw a card from the top of the deck.
+    Card draw_card(){
+        return cards[cards.size()];
+        cards.pop_back();
+    }
 };
+
+
+int main(){
+    Deck deck;
+    deck.shuffle();
+    Card drawn = deck.draw_card();
+    std::cout << drawn << '\n';
+}
